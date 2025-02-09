21ec9f476af0faac9b281884dfd61b8ca8478994
EduardoNeville <eduardoneville82@gmail.com>
2025-02-09 18:21:03 +0100

    tailwind working nothing is removed

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "@/input.css";
+
+function App() {
+  return (
+    <div className="flex flex-row">
+      <div className="w-1/3 bg-blue-500"> 
+        <p> There </p>
+      </div>
+      <div>
+        <p className="border-red-100 uppercase"> hello </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
