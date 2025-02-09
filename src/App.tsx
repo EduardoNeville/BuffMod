8cd448a9a9470feae57b63a27084caaea3f4c26f
EduardoNeville <eduardoneville82@gmail.com>
2025-02-09 18:28:43 +0100

    dashboard added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import "@/input.css";
+import DashboardPage from "./app/dashboard/page";
 
 function App() {
   return (
-    <div className="flex flex-row">
-      <div className="w-1/3 bg-blue-500"> 
-        <p> There </p>
-      </div>
-      <div>
-        <p className="border-red-100 uppercase"> hello </p>
-      </div>
-    </div>
+    <DashboardPage />
   );
 }
 
